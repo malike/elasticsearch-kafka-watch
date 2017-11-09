744c6e9f962bb9916289f91eee89a6b58e611545
malike <st.malike@gmail.com>
2017-11-09 01:04:50 +0000

    kafka producer and cron services

--- src/test/java/st/malike/elasticsearch/kafka/watch/service/KafkaProducerServiceTest.java
+++ src/test/java/st/malike/elasticsearch/kafka/watch/service/KafkaProducerServiceTest.java
@@ -1,7 +1,24 @@
 package st.malike.elasticsearch.kafka.watch.service;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
 /**
  * @autor malike_st
  */
+@RunWith(MockitoJUnitRunner.class)
 public class KafkaProducerServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testTheAwesome() throws Exception {
+        Assert.assertNotEquals("awesome","AWESOME");
+    }
 }
--- src/test/java/st/malike/elasticsearch/kafka/watch/service/TimeCronServiceTest.java
+++ src/test/java/st/malike/elasticsearch/kafka/watch/service/TimeCronServiceTest.java
@@ -1,7 +1,30 @@
 package st.malike.elasticsearch.kafka.watch.service;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * @autor malike_st
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TimeCronServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testTheAwesome() throws Exception {
+        Assert.assertNotEquals("awesome","AWESOME");
+    }
+
 }
