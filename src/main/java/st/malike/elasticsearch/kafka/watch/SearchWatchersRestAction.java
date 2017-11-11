5df8dee816b5ca5c984e6ec37825a4f60a426b6a
malike <st.malike@gmail.com>
2017-11-11 21:39:25 +0000

    for searching for watchers

--- src/main/java/st/malike/elasticsearch/kafka/watch/SearchWatchersRestAction.java
+++ src/main/java/st/malike/elasticsearch/kafka/watch/SearchWatchersRestAction.java
@@ -1,15 +1,21 @@
 package st.malike.elasticsearch.kafka.watch;
 
 import org.apache.log4j.Logger;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentHelper;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.*;
+import st.malike.elasticsearch.kafka.watch.listener.ViewWatchersListener;
 import st.malike.elasticsearch.kafka.watch.util.Enums;
 import st.malike.elasticsearch.kafka.watch.util.JSONResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static org.elasticsearch.rest.RestRequest.Method.POST;
 
@@ -29,17 +35,44 @@ public class SearchWatchersRestAction extends BaseRestHandler {
     @SuppressWarnings("unchecked")
     @Override
     protected RestChannelConsumer prepareRequest(RestRequest restRequest, NodeClient client) throws IOException {
-        JSONResponse response = new JSONResponse();
-        return channel -> {
-            response.setStatus(true);
-            response.setCount(0L);
-            response.setMessage(Enums.JSONResponseMessage.SUCCESS.toString());
-            XContentBuilder builder = channel.newBuilder();
-            builder.startObject();
-            response.toXContent(builder, restRequest);
-            builder.endObject();
-            channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder));
-        };
+        JSONResponse message =new JSONResponse();
+        Integer from = 0;
+        Integer size = 50;
+        String query=null;
+        if (restRequest.content().length() > 0) {
+            Map<String, Object> map = XContentHelper.convertToMap(restRequest.content(), false, null).v2();
+            if (!map.isEmpty()) {
+                if (map.containsKey("from")) {
+                    from = (Integer) map.get("from");
+                }
+                if (map.containsKey("limit")) {
+                    size = (Integer) map.get("limit");
+                }
+                if (map.containsKey("param")) {
+                    query = (String) map.get("param");
+                }
+            }
+        }
+        if(query==null){
+            return channel -> {
+                message.setStatus(false);
+                message.setCount(0L);
+                message.setMessage(Enums.JSONResponseMessage.MISSING_PARAM.toString());
+                XContentBuilder builder = channel.newBuilder();
+                builder.startObject();
+                message.toXContent(builder, restRequest);
+                builder.endObject();
+                channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder));
+            };
+        }
+        SearchRequestBuilder prepareSearch = client.prepareSearch(
+                ElasticKafkaWatchPlugin.getKafkaWatchElasticsearchIndex());
+        prepareSearch.setFrom(from);
+        prepareSearch.setSize(size);
+        prepareSearch.setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
+        prepareSearch.setTypes(ElasticKafkaWatchPlugin.getKafkaWatchElasticsearchType());
+        prepareSearch.setQuery(QueryBuilders.wrapperQuery(query));
+        return channel -> prepareSearch.execute(new ViewWatchersListener(channel, restRequest));
     }
 
 }
--- src/test/java/st/malike/elasticsearch/kafka/watch/ElasticKafkaWatchPluginTest.java
+++ src/test/java/st/malike/elasticsearch/kafka/watch/ElasticKafkaWatchPluginTest.java
@@ -253,7 +253,7 @@ public class ElasticKafkaWatchPluginTest {
     }
 
     @Test
-    public void searchWatchers() {
+    public void searchWatchersNoQuery() {
         given()
                 .log().all().contentType("application/json")
                 .body(new Gson().toJson(param))
@@ -263,6 +263,49 @@ public class ElasticKafkaWatchPluginTest {
                 .statusCode(200)
                 .body("status", Matchers.is(true))
                 .body("message", Matchers.is(Enums.JSONResponseMessage.SUCCESS.toString()));
+    }
+
+  @Test
+    public void searchWatchers() {
+
+      param.put("eventType", "SUBSCRIPTION");
+      param.put("description", "Send welcome notification for every subscription created");
+      param.put("channel", "SMS");
+      param.put("trigger", "INDEX_OPS");
+
+
+      given()
+              .log().all().contentType("application/json")
+              .body(new Gson().toJson(param))
+              .when()
+              .post("http://localhost:9201/_newkafkawatch")
+              .then()
+              .statusCode(200)
+              .body("status", Matchers.is(true));
+
+      //to refresh data... for fetch
+      runner.refresh();
+
+      String queryString = "{"
+                + "    \"match\": {"
+                + "      \"eventType\": \"SUBSCRIPTION\""
+                + "    }"
+                + "}";
+
+      param.put("query",queryString);
+
+
+      given()
+                .log().all().contentType("application/json")
+                .body(new Gson().toJson(param))
+                .when()
+                .post("http://localhost:9201/_listkafkawatch")
+                .then()
+                .statusCode(200)
+              .body("status", Matchers.is(true))
+              .body("count", Matchers.is(1))
+              .body("data[0].eventType", Matchers.is("SUBSCRIPTION"))
+              .body("message", Matchers.is(Enums.JSONResponseMessage.SUCCESS.toString()));
     }
 
 
