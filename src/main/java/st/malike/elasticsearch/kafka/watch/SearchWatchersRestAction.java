d5c89753161cf064371c7ba69de75a4fdced04f9
malike <st.malike@gmail.com>
2017-11-11 18:38:19 +0000

    added new restaction for searching watchers

---
+++ src/main/java/st/malike/elasticsearch/kafka/watch/SearchWatchersRestAction.java
@@ -0,0 +1,45 @@
+package st.malike.elasticsearch.kafka.watch;
+
+import org.apache.log4j.Logger;
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.rest.*;
+import st.malike.elasticsearch.kafka.watch.util.Enums;
+import st.malike.elasticsearch.kafka.watch.util.JSONResponse;
+
+import java.io.IOException;
+
+import static org.elasticsearch.rest.RestRequest.Method.POST;
+
+/**
+ * @author malike_st
+ */
+public class SearchWatchersRestAction extends BaseRestHandler {
+
+    private static Logger log = Logger.getLogger(SearchWatchersRestAction.class);
+
+    @Inject
+    public SearchWatchersRestAction(Settings settings, RestController controller) {
+        super(settings);
+        controller.registerHandler(POST, "/_searchkafkawatch", this);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected RestChannelConsumer prepareRequest(RestRequest restRequest, NodeClient client) throws IOException {
+        JSONResponse response = new JSONResponse();
+        return channel -> {
+            response.setStatus(true);
+            response.setCount(0L);
+            response.setMessage(Enums.JSONResponseMessage.SUCCESS.toString());
+            XContentBuilder builder = channel.newBuilder();
+            builder.startObject();
+            response.toXContent(builder, restRequest);
+            builder.endObject();
+            channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder));
+        };
+    }
+
+}
