836a9266794178e858e2af2da94a83aed06ff3d9
malike <st.malike@gmail.com>
2017-12-02 20:23:59 +0000

    added kafka integration,some tests still failing

--- src/main/java/st/malike/elasticsearch/kafka/watch/service/KafkaProducerService.java
+++ src/main/java/st/malike/elasticsearch/kafka/watch/service/KafkaProducerService.java
@@ -1,7 +1,13 @@
 package st.malike.elasticsearch.kafka.watch.service;
 
 import org.apache.log4j.Logger;
+import st.malike.elasticsearch.kafka.watch.ElasticKafkaWatchPlugin;
 import st.malike.elasticsearch.kafka.watch.model.KafkaEvent;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
 
 /**
  * @autor malike_st
@@ -9,9 +15,28 @@ import st.malike.elasticsearch.kafka.watch.model.KafkaEvent;
 public class KafkaProducerService {
 
     private static Logger log = Logger.getLogger(KafkaProducerService.class);
+    private static Producer<String,KafkaEvent> producer;
+
+
+    public void startKafka(){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer",
+                "org.apache.kafka.common.serializatxion.StringSerializer");
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+         producer = new KafkaProducer<>(props);
+    }
 
     public void send(KafkaEvent event) {
-
+        producer.send(new ProducerRecord<>(
+                ElasticKafkaWatchPlugin.getKafkaWatchTopic(),
+                event));
     }
 
 }
