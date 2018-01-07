42f5e3d2c6e5225d57b09c685bee1d57735a53be
malike <st.malike@gmail.com>
2018-01-07 22:45:13 +0000

    plugin config file

---
+++ src/main/java/st/malike/elasticsearch/kafka/watch/config/PluginConfig.java
@@ -0,0 +1,61 @@
+package st.malike.elasticsearch.kafka.watch.config;
+
+import org.elasticsearch.common.settings.Settings;
+
+/**
+ * @autor malike_st
+ */
+public class PluginConfig {
+
+    private static final String KAFKA_WATCH_ELASTICSEARCH_INDEX = "kafka.watch.elasticsearch.index";
+    private static final String KAFKA_WATCH_ELASTICSEARCH_TYPE = "kafka.watch.elasticsearch.type";
+    private static final String KAFKA_WATCH_BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String KAFKA_WATCH_TOPIC = "kafka.watch.topic";
+    private static final String KAFKA_WATCH_DISABLE = "kafka.watch.disable";
+    private static final String REPORT_ENGINE_ENDPOINT = "report.engine.endpoint";
+    private static final String REPORT_ENGINE_DISABLE = "report.engine.disable";
+
+
+    public PluginConfig(Settings settings)  {
+//            try {
+//                logger.info("configFile : " + configFile);
+//                prop.load(new FileInputStream(configFile));
+//
+//                logger.info("Properties : " + prop);
+//            } catch (Exception e) {
+//                logger.error("Error reading/loading configFile: " + e.getMessage(), e);
+//                throw e;
+//            }
+            }
+
+    public static String getKafkaWatchElasticsearchIndex() {
+        return KAFKA_WATCH_ELASTICSEARCH_INDEX;
+    }
+
+    public static String getKafkaWatchElasticsearchType() {
+        return KAFKA_WATCH_ELASTICSEARCH_TYPE;
+    }
+
+    public static String getKafkaWatchBootstrapServers() {
+        return KAFKA_WATCH_BOOTSTRAP_SERVERS;
+    }
+
+    public static String getKafkaWatchTopic() {
+        return KAFKA_WATCH_TOPIC;
+    }
+
+    public static Boolean getKafkaWatchDisable() {
+        return KAFKA_WATCH_DISABLE.toLowerCase().equals("false");
+    }
+
+    public static Boolean getReportEngineDisable() {
+        return REPORT_ENGINE_DISABLE.toLowerCase().equals("false");
+    }
+
+    public static String getReportEngineEndpoint() {
+        return REPORT_ENGINE_ENDPOINT;
+    }
+
+
+
+}
